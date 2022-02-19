4468c51d5287662507e0310ac8492a72d8c7ce62
Alex-Bruno <bruno-3003@hotmail.com>
2022-02-19 17:10:29 -0300

    :hammer: Add momment

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as momentTimezone from 'moment-timezone';
 
 const logger = new Logger('Main');
 const configService = new ConfigService();
@@ -22,6 +23,11 @@ async function bootstrap() {
     },
   });
 
+  Date.prototype.toJSON = function (): any {
+    return momentTimezone(this)
+      .tz('America/Sao-Paulo')
+      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  }
 
   await app.listen();
 }
